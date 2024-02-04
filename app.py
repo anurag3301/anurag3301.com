1b51a3a7f7c7117a1d497ca324bb842d8bdaa35d
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-02-04 13:32:48 +0530

    Updated directory structure

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, send_from_directory
+from flask.helpers import send_from_directory
+from flask_cors import CORS, cross_origin
+import os
+
+app = Flask(__name__, static_folder='client/build', static_url_path='')
+CORS(app)
+
+@app.route('/api/data')
+@cross_origin()
+def get_data():
+    data = {'message': 'Hello from Flask!'}
+    return jsonify(data)
+
+@app.route('/')
+@cross_origin()
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
