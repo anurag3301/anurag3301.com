bf7d1289551c8b6aaf36e0abb89a7fbac0641bb6
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-08-04 23:07:50 +0530

    Setup zola and python for blogs

--- app.py
+++ app.py
@@ -1,34 +1,26 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, abort
 from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 import os
 
-app = Flask(__name__, static_folder='client/build', static_url_path='')
+app = Flask(__name__,  static_url_path='')
 CORS(app)
-
-@app.route('/api/data')
-@cross_origin()
-def get_data():
-    data = {'message': 'Testing Github Action CICD'}
-    return jsonify(data)
-
-
-@app.route('/api/attendance')
-@cross_origin()
-def attendance_data():
-    data = {'dscc': 61.90,
-            'ml': 63.16,
-            'mad': 71.43,
-            'arvr': 54.55,
-            'vap': 70,
-            'pw': 92.31,
-            }
-    return jsonify(data)
 
 @app.route('/')
 @cross_origin()
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+def index():
+    return "hello"
+
+@app.route('/blog/', defaults={'path': 'index.html'})
+@app.route('/blog/<path:path>')
+def serve_blog_static(path):
+    file_path = os.path.join('blog/public', path)
+    if os.path.isfile(file_path):
+        return send_from_directory('blog/public', path)
+    elif os.path.isdir(file_path):
+        return send_from_directory(file_path, 'index.html')
+    else:
+        return abort(404)
 
 if __name__ == "__main__":
     app.run(debug=True)
